a073d8c13382225cb98dd1f6bd7764caff850833
vedpatil611 <vedpatil611@gmail.com>
2020-12-21 12:32:30 +0530

    Premake script fix

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,10 @@ project	"DataStructures"
 	language "C++"
 	cppdialect "C++17"
 	systemversion "latest"
+	
+	includedirs {
+		"%{prj.name}/include"
+	}
 
 	filter "configurations:Debug"
 		defines { "DEBUG" }
