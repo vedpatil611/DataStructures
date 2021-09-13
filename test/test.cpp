9fd54528b9510920101fca6eada5fbb2f55eb28b
vedpatil611 <vedpatil611@gmail.com>
2021-09-13 11:04:53 +0530

    Copy and moves logging

--- test/test.cpp
+++ test/test.cpp
@@ -2,31 +2,70 @@
 #include "../DataStructures/include/DS/Vector.h"
 #include <iostream>
 
+struct Data 
+{
+    int x, y, z;
+
+    Data()
+        :x(0), y(0), z(0)
+    {
+        printf("Created\n");
+    }
+
+    Data(int x, int y, int z)
+        :x(x), y(y), z(z)
+    {
+        printf("Created\n");
+    }
+
+    Data(const Data& other)
+        :x(other.x), y(other.y), z(other.z)
+    {
+        printf("Copy\n");
+    }
+
+    Data(Data&& other)
+        :x(other.x), y(other.y), z(other.z)
+    {
+        printf("Move\n");
+    }
+
+    Data& operator=(const Data& other)
+    {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        printf("Copy\n");
+        return *this;
+    }
+
+    Data& operator=(Data&& other)
+    {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        printf("Move\n");
+        return *this;
+    }
+
+    ~Data()
+    {
+        printf("Destroy\n");
+    }
+};
+
 int main()
 {
     {
-        ds::Array<int, 5> a;
-        a[0] = 1;
-        a[1] = 9;
-        a[2] = 3;
-        a[3] = 6;
-        a[4] = 7;
+        ds::Vector<Data> a;
+        a.push(Data());
+        a.push(Data(3, 5, 6));
+        a.push(Data(7, 1, 4));
+        a.push(Data(1, 2, 3));
+        a.push(Data(9, 8, 7));
 
-        for(int i : a) 
-            printf("%d ", i);
-        printf("\n");
-    }
-
-    {
-        ds::Vector<int> a;
-        a.push(5);
-        a.push(2);
-        a.push(6);
-        a.push(7);
-        a.push(4);
-
-        for(int i : a)
-            printf("%d ", i);
-        printf("\n");
+        //for(int i : a)
+            //printf("%d ", i);
+        //printf("\n");
     }
 }
